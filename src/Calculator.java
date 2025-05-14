d46e69151dd183a576d635535d13d8b2ad46347f
Fleuris EGBOOU <fleurismeweegboou5@gmail.com>
2025-05-14 15:18:34 +0200

    Ajout de la soustraction et mise à jour des tests

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,14 @@
-public class Calculator{
-    public static int add (int a, int b)
-    {
-        return (a+b);
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
     }
 
-public static void main (String[] args){
-    System.out.println("Somme : "+add (4,6));
-}
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Addition : " + add(4, 6));
+        System.out.println("Soustraction : " + subtract(10, 4));
+    }
 }
--- tests/CalculatorTest.java
+++ tests/CalculatorTest.java
@@ -1,10 +1,14 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class CalculatorTest{
+public class CalculatorTest {
     @Test
-    public void testAddition(){
+    public void testAddition() {
+        assertEquals(6, Calculator.add(3, 3));
+    }
 
-        assertEquals(6, Calculator.add(3,3));
+    @Test
+    public void testSubtraction() {
+        assertEquals(6, Calculator.subtract(10, 4));
     }
 }
